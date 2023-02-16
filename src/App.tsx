4144c90cf9fafb91cf5bc76e2ce9e072fdbb8521
Nicolas Schneider <nicolas.schneider@epitech.eu>
2023-02-15 21:14:04 -0500

    feat: work in progress message

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <div className="text-sky-500 text-4xl">MeteOsthoffen</div>;
+  return (
+    <div className="flex flex-col justify-center items-center pt-12">
+      <h1 className="font-bold text-neutral-800 text-2xl">MeteOsthoffen</h1>
+      <p className="text-neutral-800 mt-2">Work in progress...</p>
+    </div>
+  );
 }
 
 export default App;
